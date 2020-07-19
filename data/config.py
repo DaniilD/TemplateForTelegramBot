7307c476c1abdb689cc2adf0ba02951f48c314af
Daniil Domrachev <dom44444@mail.ru>
2020-07-19 18:04:44 +0500

    Шаблон для создания ботов для Telegramm

---
+++ data/config.py
@@ -0,0 +1,22 @@
+import os
+
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+BOT_TOKEN = "1288285121:AAHALtU-AU8yXFoMhxTzjDR8pvFKynh6h74"
+admins = [
+    354965372
+]
+
+# ip = os.getenv("ip")
+
+aiogram_redis = {
+    # 'host': ip,
+}
+
+redis = {
+    # 'address': (ip, 6379),
+    'encoding': 'utf8'
+}
